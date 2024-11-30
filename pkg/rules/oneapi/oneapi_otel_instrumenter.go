b5cfe09df50a103a9f2dcab1673401371873321b
oliver  zhang <oliver_zhangjian@163.com>
2024-11-30 15:39:23 +0800

    。。。

--- pkg/rules/oneapi/oneapi_otel_instrumenter.go
+++ pkg/rules/oneapi/oneapi_otel_instrumenter.go
@@ -15,16 +15,16 @@ func (o oneApiSpanNameExtractor) Extract(request oneApiRequest) string {
 type myTest[REQUEST any, RESPONSE any] struct {
 }
 
-func (m *myTest[REQUEST, RESPONSE]) OnStart(attributes []attribute.KeyValue, parentContext context.Context, request REQUEST) []attribute.KeyValue {
+func (m *myTest[REQUEST, RESPONSE]) OnStart(attributes []attribute.KeyValue, parentContext context.Context, request REQUEST) ([]attribute.KeyValue, context.Context) {
 	attributes = append(attributes, attribute.KeyValue{
 		Key:   attribute.Key("test-key"),
 		Value: attribute.StringValue("TestValue"),
 	})
-	return attributes
+	return attributes, parentContext
 }
 
-func (m *myTest[REQUEST, RESPONSE]) OnEnd(attributes []attribute.KeyValue, context context.Context, request REQUEST, response RESPONSE, err error) []attribute.KeyValue {
-	return attributes
+func (m *myTest[REQUEST, RESPONSE]) OnEnd(attributes []attribute.KeyValue, context context.Context, request REQUEST, response RESPONSE, err error) ([]attribute.KeyValue, context.Context) {
+	return attributes, context
 }
 
 func BuildOneApiInstrumenter() *instrumenter.InternalInstrumenter[oneApiRequest, oneApiResponse] {
